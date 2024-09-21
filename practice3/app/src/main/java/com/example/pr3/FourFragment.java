032822e06cc00842d0f691de423666c1139f3a13
Sir <57832998+mmistersir@users.noreply.github.com>
2024-09-21 11:11:17 +0300

    Add files via upload

---
+++ practice3/app/src/main/java/com/example/pr3/FourFragment.java
@@ -0,0 +1,35 @@
+package com.example.pr3;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pr3.databinding.FragmentFourBinding;
+
+
+public class FourFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        FragmentFourBinding binding = FragmentFourBinding.inflate(getLayoutInflater());
+
+        binding.button.setOnClickListener(view -> {
+            Bundle result = new Bundle();
+            result.putString("bundleKey", "Результат от нижнего фрагмента");
+            getParentFragmentManager().setFragmentResult(
+                    "requestKey", result);
+        });
+
+        return binding.getRoot();
+    }
+}
